c98e5cb547616dff1f16b9fa1d0f4ed3ae5da7b8
Junaidtahir90 <Junaidtahir90@gmail.com>
2020-10-15 17:12:44 +0500

    added summary in generic repo,added gitiginre file

--- EFCoreDemoApp/Repositries/GenericRepositry.cs
+++ EFCoreDemoApp/Repositries/GenericRepositry.cs
@@ -6,30 +6,57 @@ namespace EFCoreDemoApp.Repositries
 {
     public class GenericRepository<T> : IGenericRepository<T> where T : class
     {
+        #region variables
         public EmployeeContext _context = new EmployeeContext("Persist Security Info = false; Integrated Security = true; Initial Catalog = devDatabase; server = HAMZAPC");
         private DbSet<T> entity = null;
+        #endregion
 
+        /// <summary>
+        /// Constructor of Generic Repositry 
+        /// Sets the entity type dynamically w.r.t model type
+        /// </summary>
+       
         public GenericRepository()
         {
             entity = _context.Set<T>();
         }
 
+
+        /// <summary>
+        /// Constructor of Generic Repositry with param
+        /// Inject the EmployeeContext in GenericRepository with param
+        /// Sets the entity type dynamically w.r.t model type
+        /// </summary>
         public GenericRepository(EmployeeContext _context)
         {
             this._context = _context;
             entity = _context.Set<T>();
         }
 
+        /// <summary>
+        /// Returns List of Dynamic <T>  as per ( model type) 
+        /// </summary>
+        /// <returns>Return List of Dynamic <T>  as per (model type) </returns>
         public IEnumerable<T> GetAll()
         {
             return entity.ToList();
         }
 
+        /// <summary>
+        /// Returns List of Dynamic <T> ( model type) 
+        /// </summary>
+        /// <param>Id with respect to dynamic(model type)</param>
+        /// <returns>Retruns dynamic (model type) entity against the Id </returns>
         public T GetById(long id)
         {
             return entity.Find(id);
         }
 
+        /// <summary>
+        /// Create Record of Dynamic <T> ( model type) 
+        /// </summary>
+        /// <param>Dynamic <T> (entity) which are needs to created</param>
+        /// <returns>Dynamic (Model Type) respect to dynamic(model type) </returns>
         public T Insert(T obj)
         {
             entity.Add(obj);
@@ -37,6 +64,11 @@ namespace EFCoreDemoApp.Repositries
             return obj;
         }
 
+        /// <summary>
+        /// Update Record of Dynamic <T> ( model type) 
+        /// </summary>
+        /// <param>Dynamic <T> (entity) which are needs to updateted</param>
+        /// <returns>Return Dynamic (Model Type) respect to dynamic(model type) </returns>
         public T Update(T obj)
         {
             entity.Attach(obj);
@@ -45,9 +77,13 @@ namespace EFCoreDemoApp.Repositries
             return obj;
         }
 
+        /// <summary>
+        ///Remove of Dynamic <T> ( model type) 
+        /// </summary>
+        /// <param>Id with respect to dynamic(model type) to be removed</param>
+        /// <returns>Retruns specific String in case of sucess/failure of Deletion </returns>
         public string Delete(long id)
         {
-
             T existing = entity.Find(id);
             if (existing != null)
             {
@@ -61,9 +97,13 @@ namespace EFCoreDemoApp.Repositries
             }
         }
 
+        /// <summary>
+        ///To Save Changes w.r.t context type
+        /// </summary>
         public void Save()
         {
             _context.SaveChanges();
         }
+
     }
 }
--- EFCoreDemoApp/Repositries/IEmployeeRepositry.cs
+++ EFCoreDemoApp/Repositries/IEmployeeRepositry.cs
@@ -1,9 +1,10 @@
 ﻿using EFCoreDemoApp.Models;
+using System.Collections.Generic;
 
 namespace EFCoreDemoApp.Repositries
 {
     public interface IEmployeeRepositry : IGenericRepository<Employee>
     {
-        
+        List<ResultDTO> DuplicateRecords();
     }
 }
